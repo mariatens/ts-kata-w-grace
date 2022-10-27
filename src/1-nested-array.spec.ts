14cb65f2f67da1abdae5395701d9c90b4a0d0abc
Maria <mtens17@gmail.com>
2022-10-27 10:06:12 +0100

    Finish helper function and tests passing

--- src/1-nested-array.spec.ts
+++ src/1-nested-array.spec.ts
@@ -1,1 +1,6 @@
-import getMaximums from './1-nested-array';
+// import getMaximums from './1-nested-array';
+import getMaximum from './1-nested-array';
+
+test("getMaximum returns biggest number from a number array", () => {
+    expect(getMaximum([12,3])).toBe(12);
+});
--- src/1-nested-array.ts
+++ src/1-nested-array.ts
@@ -1,6 +1,19 @@
-function getMaximums(inputArr: number[][]): number[] {
-    return 
-}
-console.log(getMaximums([[1,2], [2,3]], [2,3]))
+// function getMaximums(inputArrs: number[][]): number[] {
+    
+//     return 
+// }
 
-export default getMaximums;
+// helper function
+function getMaximum(inputArr: number[]): number{
+    let maxSoFar = inputArr[0]
+    for (const num of inputArr){
+        if (num > maxSoFar){
+            maxSoFar = num
+        }
+    }
+    return maxSoFar
+}
+
+// console.log(getMaximums([[1,2], [2,3]]), [2,3])
+
+export default getMaximum;
