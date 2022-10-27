c6b7182bad61a710a1e08109e7456f01600a1716
Maria <mtens17@gmail.com>
2022-10-27 10:27:24 +0100

    Unskip helper tests

--- src/1-nested-array.spec.ts
+++ src/1-nested-array.spec.ts
@@ -1,9 +1,9 @@
 import getMaximums from './1-nested-array';
 
 test("getMaximums returns number array  with biggest values from each subarray", () => {
-    expect(getMaximums([[12,3]])).toStrictEqual([12]);
+    expect(getMaximums([[12,3]])).toEqual([12]);
 });
 
 test("getMaximums returns number array  with biggest values from each subarray", () => {
-    expect(getMaximums([[10, 70, 30], [40], [20, -30, 80]])).toStrictEqual([70, 40, 80]);
+    expect(getMaximums([[10, 70, 30], [40], [20, -30, 80]])).toEqual([70, 40, 80]);
 });
